a90d96f6fb0dc8fea7547b148286eefd9521d913
lennartolesen <lbolesen37@gmail.com>
2024-09-20 10:32:02 +0000

    h

--- test_packages.R
+++ test_packages.R
@@ -1,6 +1,3 @@
 library(httr2)
-<<<<<<< HEAD
-# bla bla some comments
-=======
-# bla bla some other comments
->>>>>>> fa13e78d7e8ad348eca133da6b7460ec5082e6b8
+# bla bla some comments now fixed bla bla some other comments
+
